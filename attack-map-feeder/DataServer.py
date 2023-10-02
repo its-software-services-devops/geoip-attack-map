ea6f60a7ed0a05b9a6bffdfc4e885064f74f8f75
pjames <pjames@gmail.com>
2023-10-02 17:00:21 +0700

    Adjusted delay

--- attack-map-feeder/DataServer.py
+++ attack-map-feeder/DataServer.py
@@ -320,6 +320,7 @@ def main():
                 sleep(.05)
                 syslog_file.seek(where)
             else:
+                sleep(.01)
                 syslog_data_dict = parse_syslog(line)
                 if syslog_data_dict:
                     ip_db_unclean = parse_maxminddb(db_path, syslog_data_dict['src_ip'])
